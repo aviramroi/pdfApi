dd2905b2ff8c67ca3372859f19dd498b3696ba34
shitot handasi <shitotazure@gmail.com>
2020-03-28 00:34:49 +0300

    working version

--- src/app.js
+++ src/app.js
@@ -21,12 +21,12 @@ function createApp() {
     app.use(morgan('dev'));
   }
 
-  if (!config.ALLOW_HTTP) {
-    logger.info('All requests require HTTPS.');
-    app.use(requireHttps());
-  } else {
-    logger.info('ALLOW_HTTP=true, unsafe requests are allowed. Don\'t use this in production.');
-  }
+  // if (!config.ALLOW_HTTP) {
+  //   logger.info('All requests require HTTPS.');
+  //   app.use(requireHttps());
+  // } else {
+  //   logger.info('ALLOW_HTTP=true, unsafe requests are allowed. Don\'t use this in production.');
+  // }
 
   const corsOpts = {
     origin: config.CORS_ORIGIN,
@@ -55,3 +55,9 @@ function createApp() {
 }
 
 module.exports = createApp;
+
+
+
+/*
+sudo apt-get install gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+*/
